aec0b8e1fa3f5a3ac155d7167b33f65a3644b176
Balula12 <gugabasigb1@gmail.com>
2024-03-22 14:08:42 -0300

    feat: fonts, color, global styles, eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./@tipos/global";
 
 export function App() {
 
@@ -12,7 +13,7 @@ export function App() {
    <Button variant="success"/>
    <Button variant="danger"/>
    <Button/>
-
+    <GlobalStyle />
    </ThemeProvider>
   )
 }
