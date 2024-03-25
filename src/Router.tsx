90b53d9d444d4d21da960d36a6715829330cb2e1
Balula12 <gugabasigb1@gmail.com>
2024-03-25 16:58:58 -0300

    feat: logo & icons

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home';
 import { History } from './pages/History';
-import { DefaultLayout } from './layouts/Default.layout';
+import { DefaultLayout } from './layouts/DefaultLayout';
+
 
 export function Router() {
     return (
